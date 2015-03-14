1c92e474cc63716a472b6d0d81d2c2a9f74da33a
Liubov <liubov@groupon.ru>
2015-03-14 23:37:47 +0300

    Update rasp.js

--- templates/rasp.js
+++ templates/rasp.js
@@ -8,7 +8,7 @@ var dest_lat = {{ locations[direction]["loc"][0] }};
 var dest_long = {{ locations[direction]["loc"][1] }};
 
 function toggleCollapse(id) {
-    var el = document.getElementById(id).children[1];
+    var el = document.getElementById(id);
     el.classList.toggle('collapsed');
 }
 
